e406859cf837ade8e9bc2a584bae20c46f58d8e0
Kalyanee <kalyaneedofficial@gmail.com>
2025-09-05 21:48:26 +0530

    Updated event popup for The Great Get Together on 11th Sept

--- src/Pages/HomePage.tsx
+++ src/Pages/HomePage.tsx
@@ -32,13 +32,12 @@ const HomePage: React.FC<HomePageProps> = ({ isMenuOpen }) => {
       <Partners />
       <TeamSection />
       
-      {/*
+      {
       <EventModal
         isOpen={showModal}
         onClose={() => setShowModal(false)}
-        registrationUrl="https://unstop.com/o/hGuVyrW?lb=RfihYaz&utm_medium=Share&utm_source=shortUrl"
       />
-      */}
+      }
     </div>
   )
 }
